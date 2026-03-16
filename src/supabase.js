e9b54daaeef5be9de252ec0a5a34a9fcf0254b5b
naota-max <nao.tagucci@gmail.com>
2026-03-16 18:27:26 +0900

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -4,4 +4,3 @@ const SUPABASE_URL = 'https://qybothwckwnjosulwyzm.supabase.co'
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF5Ym90aHdja3duam9zdWx3eXptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM2MDI0MTYsImV4cCI6MjA4OTE3ODQxNn0.ebTEs1b_yeKDQJ2qkgEsYOFYY5tqpQSHjf4YHBKyuwE'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
-alter table tasks add column if not exists relayed_from text default '';
